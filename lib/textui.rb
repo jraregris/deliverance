709ebfa574f96939e16f524f88efbcd0b5027138
Oddmund <oddmunds@elmo.(none)>
2009-12-21 23:16:52 +0100

    Gave files saner names.

--- lib/textui.rb
+++ lib/textui.rb
@@ -3,12 +3,13 @@ module Deliverance
     def initialize(io)
       @io = io
     end
+
     def set_papers(i)
       @io.puts "Papers: " + i.to_s
     end
 
     def papers
-      return 10
+      10
     end
 
     def points
